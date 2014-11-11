9d95ab972e2cd5621182daba3ee378e391eef73e
Michel Hartmann <M.Hartmann@flyeralarm.de>
2014-11-11 11:51:59 +0100

    Fixed: Removed output from unit test

--- src/test/java/org/cip4/lib/xjdf/util/IDGeneratorUtilTest.java
+++ src/test/java/org/cip4/lib/xjdf/util/IDGeneratorUtilTest.java
@@ -1,16 +1,6 @@
-/**
- * All rights reserved by
- * 
- * flyeralarm GmbH
- * Alfred-Nobel-Straße 18
- * 97080 Würzburg
- *
- * Email: info@flyeralarm.com
- * Website: http://www.flyeralarm.com
- */
 package org.cip4.lib.xjdf.util;
 
-import junit.framework.Assert;
+import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.Before;
@@ -18,114 +8,113 @@ import org.junit.Test;
 
 /**
  * JUnit test for IDGenerator
+ *
  * @author s.meissner
- * @date 09.01.2013
+ * @author m.hartmann
  */
 public class IDGeneratorUtilTest {
 
-	/**
-	 * Setup unit test.
-	 * @throws java.lang.Exception
-	 */
-	@Before
-	public void setUp() throws Exception {
-	}
+    /**
+     * Setup unit test.
+     *
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
 
-	/**
-	 * Tear down unit test.
-	 * @throws java.lang.Exception
-	 */
-	@After
-	public void tearDown() throws Exception {
-	}
+    /**
+     * Tear down unit test.
+     *
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+    }
 
-	/**
-	 * Test method for {@link org.cip4.lib.xjdf.util.IDGeneratorUtil#generateID(java.lang.String)}.
-	 */
-	@Test
-	public void testGenerateID1_PREFIX() {
+    /**
+     * Test method for {@link org.cip4.lib.xjdf.util.IDGeneratorUtil#generateID(java.lang.String)}.
+     */
+    @Test
+    public void testGenerateID1_PREFIX() {
 
-		// arrange
-		final String prefix = "ID";
+        // arrange
+        final String prefix = "ID";
 
-		// act
-		String result = IDGeneratorUtil.generateID(prefix);
+        // act
+        String result = IDGeneratorUtil.generateID(prefix);
 
-		// assert
-		System.out.println(result);
-		Assert.assertTrue("ID is wrong.", result.startsWith(prefix));
-	}
+        // assert
+        assertTrue("ID is wrong.", result.startsWith(prefix));
+    }
 
-	/**
-	 * Test method for {@link org.cip4.lib.xjdf.util.IDGeneratorUtil#generateID(java.lang.String)}.
-	 */
-	@Test
-	public void testGenerateID2_PREFIX() {
+    /**
+     * Test method for {@link org.cip4.lib.xjdf.util.IDGeneratorUtil#generateID(java.lang.String)}.
+     */
+    @Test
+    public void testGenerateID2_PREFIX() {
 
-		// arrange
+        // arrange
 
-		// act
-		String result = IDGeneratorUtil.generateID("pre");
+        // act
+        String result = IDGeneratorUtil.generateID("pre");
 
-		// assert
-		System.out.println(result);
-		Assert.assertTrue("ID is wrong.", result.startsWith("PRE_"));
-	}
+        // assert
+        assertTrue("ID is wrong.", result.startsWith("PRE_"));
+    }
 
-	/**
-	 * Test method for {@link org.cip4.lib.xjdf.util.IDGeneratorUtil#generateID(java.lang.String)}.
-	 */
-	@Test
-	public void testGenerateID3_PREFIX() {
+    /**
+     * Test method for {@link org.cip4.lib.xjdf.util.IDGeneratorUtil#generateID(java.lang.String)}.
+     */
+    @Test
+    public void testGenerateID3_PREFIX() {
 
-		// arrange
+        // arrange
 
-		// act
-		String result = IDGeneratorUtil.generateID("");
+        // act
+        String result = IDGeneratorUtil.generateID("");
 
-		// assert
-		System.out.println(result);
-		Assert.assertTrue("ID is wrong.", result.startsWith("ID_"));
-	}
+        // assert
+        assertTrue("ID is wrong.", result.startsWith("ID_"));
+    }
 
-	/**
-	 * Test method for {@link org.cip4.lib.xjdf.util.IDGeneratorUtil#generateID(java.lang.String)}.
-	 */
-	@Test
-	public void testGenerateID4_PREFIX() {
+    /**
+     * Test method for {@link org.cip4.lib.xjdf.util.IDGeneratorUtil#generateID(java.lang.String)}.
+     */
+    @Test
+    public void testGenerateID4_PREFIX() {
 
-		// arrange
-		final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-		int[] array = new int[CHARACTERS.length()];
+        // arrange
+        final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        int[] array = new int[CHARACTERS.length()];
 
-		// act
-		for (int i = 0; i < 4000; i++) {
-			String result = IDGeneratorUtil.generateID("");
+        // act
+        for (int i = 0; i < 4000; i++) {
+            String result = IDGeneratorUtil.generateID("");
 
-			String id = result.split("_")[1];
+            String id = result.split("_")[1];
 
-			for (int n = 0; n < id.length(); n++) {
-				int m = CHARACTERS.indexOf(id.charAt(n));
-				array[m]++;
-			}
-		}
+            for (int n = 0; n < id.length(); n++) {
+                int m = CHARACTERS.indexOf(id.charAt(n));
+                array[m]++;
+            }
+        }
 
-		for (int i = 0; i < 4000; i++) {
-			String result = IDGeneratorUtil.generateID("abc");
+        for (int i = 0; i < 4000; i++) {
+            String result = IDGeneratorUtil.generateID("abc");
 
-			String id = result.split("_")[1];
+            String id = result.split("_")[1];
 
-			for (int n = 0; n < id.length(); n++) {
-				int m = CHARACTERS.indexOf(id.charAt(n));
-				array[m]++;
-			}
-		}
+            for (int n = 0; n < id.length(); n++) {
+                int m = CHARACTERS.indexOf(id.charAt(n));
+                array[m]++;
+            }
+        }
 
-		// assert
-		for (int i = 0; i < array.length; i++) {
-			System.out.println("Character " + CHARACTERS.charAt(i) + ": " + array[i]);
-			Assert.assertFalse("Character '" + CHARACTERS.charAt(i) + "' isn't used.", array[i] == 0);
-		}
-	}
+        // assert
+        for (int i = 0; i < array.length; i++) {
+            assertFalse("Character '" + CHARACTERS.charAt(i) + "' isn't used.", array[i] == 0);
+        }
+    }
 
 }
