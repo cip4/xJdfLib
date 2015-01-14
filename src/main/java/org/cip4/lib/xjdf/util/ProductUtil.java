8ecd928a6046b29338834ab709ac814a8d22f067
Michel Hartmann <M.Hartmann@flyeralarm.de>
2015-01-14 10:34:21 +0100

    * Introduced method for simply retrieving a specific intent of a product

---
+++ src/main/java/org/cip4/lib/xjdf/util/ProductUtil.java
@@ -0,0 +1,36 @@
+package org.cip4.lib.xjdf.util;
+
+import org.cip4.lib.xjdf.schema.Intent;
+import org.cip4.lib.xjdf.schema.IntentType;
+import org.cip4.lib.xjdf.schema.Product;
+
+/**
+ * Utility for performing reoccuring operations on products.
+ */
+public final class ProductUtil {
+
+    /**
+     * Private constructor for utility class.
+     */
+    private ProductUtil() {
+    }
+
+    /**
+     * Retrieve a specific intent from a product.
+     *
+     * @param product Product te read intent from.
+     * @param intentType Type of the intent.
+     * @param <T> Type of the intent.
+     * @return Requested Intent or null if no matching intent was found.
+     */
+    @SuppressWarnings("unchecked")
+    public static <T extends IntentType> T getIntent(final Product product, final Class<T> intentType) {
+        for (Intent intent : product.getIntent()) {
+            if (intent.getIntentType().getValue().getClass().isAssignableFrom(intentType)) {
+                return (T) intent.getIntentType().getValue();
+            }
+        }
+        return null;
+    }
+
+}
---
+++ src/test/java/org/cip4/lib/xjdf/util/ProductUtilTest.java
@@ -0,0 +1,33 @@
+package org.cip4.lib.xjdf.util;
+
+import org.cip4.lib.xjdf.schema.ColorIntent;
+import org.cip4.lib.xjdf.schema.LayoutIntent;
+import org.cip4.lib.xjdf.schema.Product;
+import org.junit.Test;
+import org.cip4.lib.xjdf.builder.ProductBuilder;
+
+import static org.junit.Assert.*;
+
+public class ProductUtilTest {
+
+    @Test
+    public void getIntent() throws Exception {
+        ProductBuilder productBuilder = new ProductBuilder();
+        ColorIntent colorIntent = new ColorIntent();
+        productBuilder.addIntent(new LayoutIntent());
+        productBuilder.addIntent(colorIntent);
+        Product product = productBuilder.build();
+
+        assertSame(colorIntent, ProductUtil.getIntent(product, ColorIntent.class));
+    }
+
+    @Test
+    public void getIntentReturnsNull() throws Exception {
+        ProductBuilder productBuilder = new ProductBuilder();
+        ColorIntent colorIntent = new ColorIntent();
+        productBuilder.addIntent(colorIntent);
+        Product product = productBuilder.build();
+
+        assertNull(ProductUtil.getIntent(product, LayoutIntent.class));
+    }
+}
