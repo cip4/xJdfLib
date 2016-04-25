8d57881475e720c82e6a4de3bb6b3622a173fcd2
s.meissner <s.meissner@flyeralarm.de>
2016-04-25 11:34:00 +0200

    CheckStyle Improvements

--- src/main/java/org/cip4/lib/xjdf/partition/PartitionManager.java
+++ src/main/java/org/cip4/lib/xjdf/partition/PartitionManager.java
@@ -15,12 +15,12 @@ import org.cip4.lib.xjdf.schema.Resource;
 import org.cip4.lib.xjdf.schema.ResourceSet;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Manager class containing all the partitioning logic.
+ *
  * @author Stefan Meissner.
  */
 public class PartitionManager {
@@ -28,39 +28,43 @@ public class PartitionManager {
     /**
      * Identifies and returns a Resource within a ResourceSet using parition keys. <br />
      * This is the official CIP4 reference implementation of the XJDF partitioning algorithm.
+     *
      * @param resourceSet The ResourceSet containing a set of partitioned Resources.
      * @param part The given Partition Keys used to identify a particular Resource
+     *
      * @return The Resource identified using partition keys.
+     * @throws IllegalAccessException Is thrown in case the partition isn't accessible in Part class.
+     * @throws NoSuchFieldException Is thrown in case the partition key wasn't found in Part class.
      */
-    public Resource getResourceByPart(ResourceSet resourceSet, Part part)
+    public final Resource getResourceByPart(final ResourceSet resourceSet, final Part part)
         throws IllegalAccessException, NoSuchFieldException {
 
         // iterate over all parts in all resources from top to bottom
-        for(Resource resource: resourceSet.getResource()) {
+        for (Resource resource : resourceSet.getResource()) {
 
             // catch all, if no part is specified in resource
-            if(resource.getPart().size() == 0) {
+            if (resource.getPart().size() == 0) {
                 return resource;
             }
 
-            for(Part resPart: resource.getPart()) {
+            for (Part resPart : resource.getPart()) {
 
                 // check if all partition keys are in the set of given keys
                 Set<String> givenKeys = getPartitionKeys(part);
                 Set<String> resourceKeys = getPartitionKeys(resPart);
 
-                if(givenKeys.containsAll(resourceKeys)) {
+                if (givenKeys.containsAll(resourceKeys)) {
 
                     // compare values of keys
                     boolean isSelected = true;
 
-                    for(String resourceKey: resourceKeys) {
-                        if(isSelected) {
+                    for (String resourceKey : resourceKeys) {
+                        if (isSelected) {
                             isSelected = comparePartKeyValues(resourceKey, resPart, part);
                         }
                     }
 
-                    if(isSelected) {
+                    if (isSelected) {
                         return resource;
                     }
                 }
@@ -75,12 +79,16 @@ public class PartitionManager {
      * Compares the value of the resource partition key with the value of the given partition key.
      * If the values are equal or the resource partition key value is a subset of the given one the result is 'true'.
      * Otherwise the result is 'false'.
+     *
      * @param fieldName The field name of the fields to be compared.
      * @param resourcePart The part element of the resource.
      * @param givenPart The given part element.
+     *
      * @return True in case the resource partition key value is equal or a subset of the given partition key value.
+     * @throws IllegalAccessException Is thrown in case the partition isn't accessible in Part class.
+     * @throws NoSuchFieldException Is thrown in case the partition key wasn't found in Part class.
      */
-    private boolean comparePartKeyValues (String fieldName, Part resourcePart, Part givenPart)
+    private boolean comparePartKeyValues(final String fieldName, final Part resourcePart, final Part givenPart)
         throws NoSuchFieldException, IllegalAccessException {
 
         Field field = Part.class.getDeclaredField(fieldName);
@@ -95,14 +103,16 @@ public class PartitionManager {
 
     /**
      * Get list of active partition keys of a part element.
+     *
      * @param part The Part element to be analyzed.
+     *
      * @return List of active parition keys.
-     * @throws IllegalAccessException
+     * @throws IllegalAccessException Is thrown in case the partition isn't accessible in Part class.
      */
-    private Set<String> getPartitionKeys(Part part) throws IllegalAccessException {
+    private Set<String> getPartitionKeys(final Part part) throws IllegalAccessException {
         Set<String> partKeys = new HashSet<>();
 
-        if(part != null) {
+        if (part != null) {
             Field[] fields = Part.class.getDeclaredFields();
 
             for (Field field : fields) {
--- src/test/java/org/cip4/lib/xjdf/partition/PartitionManagerTest.java
+++ src/test/java/org/cip4/lib/xjdf/partition/PartitionManagerTest.java
@@ -9,54 +9,65 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Arrays;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
-import static org.junit.Assert.*;
 
 /**
  * JUnit Test case for PartitionManager object.
+ *
  * @author s.meissner
- * @date 24.04.2016
  */
 public class PartitionManagerTest {
 
-    private final static String RES_EXPOSED_MEDIA = "/org/cip4/lib/xjdf/partition/partition-exposedmedia.xjdf";
+    /**
+     * NodeFactory for creating XJDF Nodes.
+     */
+    private static final XJdfNodeFactory NF = new XJdfNodeFactory();
 
-    private final static XJdfNodeFactory NF = new XJdfNodeFactory();
-
+    /**
+     * The current Partition Manager.
+     */
     private PartitionManager partitionManager;
 
+    /**
+     * The current Resource Set.
+     */
     private ResourceSet resourceSet;
 
     /**
      * Setup unit test.
-     * @throws java.lang.Exception
+     *
+     * @throws Exception Catch all Exception.
      */
     @Before
-    public void setUp() throws Exception {
+    public final void setUp() throws Exception {
         partitionManager = new PartitionManager();
         resourceSet = NF.createResourceSet();
     }
 
     /**
      * Tear down unit test.
-     * @throws java.lang.Exception
+     *
+     * @throws Exception Catch all Exception
      */
     @After
-    public void tearDown() throws Exception {
+    public final void tearDown() throws Exception {
     }
 
     /**
      * The second Resource in ResourceSet has no Part element (catch all).
+     *
+     * @throws Exception Catch all Exception
      */
     @Test
-    public void testGetResourceByPart_EmptyPart() throws Exception {
+    public final void testGetResourceByPartEmptyPart() throws Exception {
         // arrange
         createResource("CYAN_FRONT");
-        createPart_SeparationSide("Cyan", EnumSide.FRONT, "CYAN_FRONT");
+        createPartSeparationSide("Cyan", EnumSide.FRONT, "CYAN_FRONT");
 
         createResource("CYAN_BACK");
-        createPart_SeparationSide("Cyan", EnumSide.BACK, "CYAN_BACK");
+        createPartSeparationSide("Cyan", EnumSide.BACK, "CYAN_BACK");
 
         // act / assert
         Part part;
@@ -64,104 +75,152 @@ public class PartitionManagerTest {
         assertNull("Resource is not null.", partitionManager.getResourceByPart(resourceSet, null));
         assertNull("Resource is not null.", partitionManager.getResourceByPart(resourceSet, NF.createPart()));
 
-        part = createPart_SeparationSide("Cyan", null, null);
+        part = createPartSeparationSide("Cyan", null, null);
         assertNull("Resource is not null.", partitionManager.getResourceByPart(resourceSet, part));
     }
 
     /**
      * The second Resource in ResourceSet has no Part element (second resource does catch all).
+     *
+     * @throws Exception Catch all Exception
      */
     @Test
-    public void testGetResourceByPart_CatchAllMiddle() throws Exception {
+    public final void testGetResourceByPartCatchAllMiddle() throws Exception {
         // arrange
         createResource("CYAN_FRONT");
-        createPart_SeparationSide("Cyan", EnumSide.FRONT, "CYAN_FRONT");
+        createPartSeparationSide("Cyan", EnumSide.FRONT, "CYAN_FRONT");
 
         createResource("CYAN_CATCH_ALL");
 
         createResource("CYAN_BACK");
-        createPart_SeparationSide("Cyan", EnumSide.BACK, "CYAN_BACK");
+        createPartSeparationSide("Cyan", EnumSide.BACK, "CYAN_BACK");
 
         // act / assert
         Part part;
 
-        part = createPart_SeparationSide("Cyan", EnumSide.FRONT, null);
-        assertEquals("Resource ID is wrong.", "CYAN_FRONT", partitionManager.getResourceByPart(resourceSet, part).getID());
+        part = createPartSeparationSide("Cyan", EnumSide.FRONT, null);
+        assertEquals(
+            "Resource ID is wrong.",
+            "CYAN_FRONT",
+            partitionManager.getResourceByPart(resourceSet, part).getID()
+        );
 
-        part = createPart_SeparationSide("Cyan", EnumSide.BACK, null);
-        assertEquals("Resource ID is wrong.", "CYAN_CATCH_ALL", partitionManager.getResourceByPart(resourceSet, part).getID());
+        part = createPartSeparationSide("Cyan", EnumSide.BACK, null);
+        assertEquals(
+            "Resource ID is wrong.",
+            "CYAN_CATCH_ALL",
+            partitionManager.getResourceByPart(resourceSet, part).getID()
+        );
     }
 
     /**
-     * Each resource contains one unique partition key
+     * Each resource contains one unique partition key.
+     *
+     * @throws Exception Catch all Exception
      */
     @Test
-    public void testGetResourceByPart_OnePartPerResource() throws Exception {
+    public final void testGetResourceByPartOnePartPerResource() throws Exception {
         // arrange
         createResource("CYAN_FRONT");
-        createPart_SeparationSide("Cyan", EnumSide.FRONT, "CYAN_FRONT");
+        createPartSeparationSide("Cyan", EnumSide.FRONT, "CYAN_FRONT");
 
         createResource("CYAN_BACK");
-        createPart_SeparationSide("Cyan", EnumSide.BACK, "CYAN_BACK");
+        createPartSeparationSide("Cyan", EnumSide.BACK, "CYAN_BACK");
 
         createResource("BLACK_FRONT");
-        createPart_SeparationSide("Black", EnumSide.FRONT, "BLACK_FRONT");
+        createPartSeparationSide("Black", EnumSide.FRONT, "BLACK_FRONT");
 
         createResource("BLACK_BACK");
-        createPart_SeparationSide("Black", EnumSide.BACK, "BLACK_BACK");
+        createPartSeparationSide("Black", EnumSide.BACK, "BLACK_BACK");
 
         // act / assert
         Part part;
 
-        part = createPart_SeparationSide("Cyan", EnumSide.FRONT, null);
-        assertEquals("Resource ID is wrong.", "CYAN_FRONT", partitionManager.getResourceByPart(resourceSet, part).getID());
+        part = createPartSeparationSide("Cyan", EnumSide.FRONT, null);
+        assertEquals(
+            "Resource ID is wrong.",
+            "CYAN_FRONT",
+            partitionManager.getResourceByPart(resourceSet, part).getID()
+        );
 
-        part = createPart_SeparationSide("Cyan", EnumSide.BACK, null);
-        assertEquals("Resource ID is wrong.", "CYAN_BACK", partitionManager.getResourceByPart(resourceSet, part).getID());
+        part = createPartSeparationSide("Cyan", EnumSide.BACK, null);
+        assertEquals(
+            "Resource ID is wrong.",
+            "CYAN_BACK",
+            partitionManager.getResourceByPart(resourceSet, part).getID()
+        );
 
-        part = createPart_SeparationSide("Black", EnumSide.FRONT, null);
-        assertEquals("Resource ID is wrong.", "BLACK_FRONT", partitionManager.getResourceByPart(resourceSet, part).getID());
+        part = createPartSeparationSide("Black", EnumSide.FRONT, null);
+        assertEquals(
+            "Resource ID is wrong.",
+            "BLACK_FRONT",
+            partitionManager.getResourceByPart(resourceSet, part).getID()
+        );
 
-        part = createPart_SeparationSide("Black", EnumSide.BACK, null);
-        assertEquals("Resource ID is wrong.", "BLACK_BACK", partitionManager.getResourceByPart(resourceSet, part).getID());
+        part = createPartSeparationSide("Black", EnumSide.BACK, null);
+        assertEquals(
+            "Resource ID is wrong.",
+            "BLACK_BACK",
+            partitionManager.getResourceByPart(resourceSet, part).getID()
+        );
     }
 
     /**
-     * Each resource contains two unique partition keys
+     * Each resource contains two unique partition keys.
+     *
+     * @throws Exception Catch all Exception
      */
     @Test
-    public void testGetResourceByPart_TwoPartsPerResource() throws Exception {
+    public final void testGetResourceByPartTwoPartsPerResource() throws Exception {
         // arrange
         createResource("CYAN_FRONT_BACK");
-        createPart_SeparationSide("Cyan", EnumSide.FRONT, "CYAN_FRONT_BACK");
-        createPart_SeparationSide("Cyan", EnumSide.BACK, "CYAN_FRONT_BACK");
+        createPartSeparationSide("Cyan", EnumSide.FRONT, "CYAN_FRONT_BACK");
+        createPartSeparationSide("Cyan", EnumSide.BACK, "CYAN_FRONT_BACK");
 
         createResource("BLACK_FRONT_BACK");
-        createPart_SeparationSide("Black", EnumSide.FRONT, "BLACK_FRONT_BACK");
-        createPart_SeparationSide("Black", EnumSide.BACK, "BLACK_FRONT_BACK");
+        createPartSeparationSide("Black", EnumSide.FRONT, "BLACK_FRONT_BACK");
+        createPartSeparationSide("Black", EnumSide.BACK, "BLACK_FRONT_BACK");
 
         // act / assert
         Part part;
 
-        part = createPart_SeparationSide("Cyan", EnumSide.FRONT, null);
-        assertEquals("Resource ID is wrong.", "CYAN_FRONT_BACK", partitionManager.getResourceByPart(resourceSet, part).getID());
+        part = createPartSeparationSide("Cyan", EnumSide.FRONT, null);
+        assertEquals(
+            "Resource ID is wrong.",
+            "CYAN_FRONT_BACK",
+            partitionManager.getResourceByPart(resourceSet, part).getID()
+        );
 
-        part = createPart_SeparationSide("Cyan", EnumSide.BACK, null);
-        assertEquals("Resource ID is wrong.", "CYAN_FRONT_BACK", partitionManager.getResourceByPart(resourceSet, part).getID());
+        part = createPartSeparationSide("Cyan", EnumSide.BACK, null);
+        assertEquals(
+            "Resource ID is wrong.",
+            "CYAN_FRONT_BACK",
+            partitionManager.getResourceByPart(resourceSet, part).getID()
+        );
 
-        part = createPart_SeparationSide("Black", EnumSide.FRONT, null);
-        assertEquals("Resource ID is wrong.", "BLACK_FRONT_BACK", partitionManager.getResourceByPart(resourceSet, part).getID());
+        part = createPartSeparationSide("Black", EnumSide.FRONT, null);
+        assertEquals(
+            "Resource ID is wrong.",
+            "BLACK_FRONT_BACK",
+            partitionManager.getResourceByPart(resourceSet, part).getID()
+        );
 
-        part = createPart_SeparationSide("Black", EnumSide.BACK, null);
-        assertEquals("Resource ID is wrong.", "BLACK_FRONT_BACK", partitionManager.getResourceByPart(resourceSet, part).getID());
+        part = createPartSeparationSide("Black", EnumSide.BACK, null);
+        assertEquals(
+            "Resource ID is wrong.",
+            "BLACK_FRONT_BACK",
+            partitionManager.getResourceByPart(resourceSet, part).getID()
+        );
     }
 
     /**
      * Add a part element containing a Separation and a Side attribute to a given Resource object.
+     *
      * @param side The side to be added.
      * @param resourceId The id of the given Resource object.
+     * @return The initialized Part object.
      */
-    private Part createPart_Side(EnumSide side, String resourceId) {
+    private Part createPartSide(final EnumSide side, final String resourceId) {
 
         // create part
         Part part = NF.createPart();
@@ -176,11 +235,13 @@ public class PartitionManagerTest {
 
     /**
      * Add a part element containing a Separation and a Side attribute to a given Resource object.
+     *
      * @param separation The sepeartion to be added.
      * @param side The side to be added.
      * @param resourceId The id of the given Resource object.
+     * @return The initialized Part object.
      */
-    private Part createPart_SeparationSide(String separation, EnumSide side, String resourceId) {
+    private Part createPartSeparationSide(final String separation, final EnumSide side, final String resourceId) {
 
         // create part
         Part part = NF.createPart();
@@ -196,17 +257,21 @@ public class PartitionManagerTest {
 
     /**
      * Add a part element containing a Separation and a Side attribute to a given Resource object.
+     *
      * @param separation The sepeartion to be added.
      * @param side The side to be added.
      * @param resourceId The id of the given Resource object.
+     * @param partVersion The part Version of the given Resource object
      */
-    private Part createPart_SeparationSideVersion(String separation, EnumSide side, String resourceId, String... partVersions) {
+    private Part createPartSeparationSideVersion(
+        final String separation, final EnumSide side, final String resourceId, final String partVersion
+    ) {
 
         // create part
         Part part = NF.createPart();
         part.setSeparation(separation);
         part.setSide(side);
-        part.getPartVersion().addAll(Arrays.asList(partVersions));
+        part.getPartVersion().add(partVersion);
 
         // add to referenced resource
         addPartToResource(resourceId, part);
@@ -216,12 +281,13 @@ public class PartitionManagerTest {
     }
 
     /**
-     * Add part to a resrouce identified by resourceId
+     * Add part to a resrouce identified by resourceId.
+     *
      * @param resourceId Identifier of the resource.
      * @param part The part element to be added.
      */
-    private void addPartToResource(String resourceId, Part part) {
-        for(Resource resource: resourceSet.getResource()) {
+    private void addPartToResource(final String resourceId, final Part part) {
+        for (Resource resource : resourceSet.getResource()) {
             if (resource.getID().equals(resourceId)) {
                 resource.getPart().add(part);
             }
@@ -230,10 +296,12 @@ public class PartitionManagerTest {
 
     /**
      * Create a new Resource element.
+     *
      * @param id Id of the new Resource elemenet.
+     *
      * @return The created Resource element.
      */
-    private Resource createResource(String id) {
+    private Resource createResource(final String id) {
         Resource resource = NF.createResource();
         resource.setID(id);
 
