9b7c596f0a414de332ceeb5dcd1baea8152a4fca
Stefan Meissner <stefan.meissner@vista.com>
2023-05-12 10:04:19 +0200

    fix

--- src/main/java/org/cip4/lib/xjdf/FinalProduct.java
+++ src/main/java/org/cip4/lib/xjdf/FinalProduct.java
@@ -71,7 +71,7 @@ public class FinalProduct {
      */
     public void addIntents(ProductIntent... productIntents) {
         for(ProductIntent productIntent : productIntents) {
-            addIntent(productIntent)
+            addIntent(productIntent);
         }
     }
 
